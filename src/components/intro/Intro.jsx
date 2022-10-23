fb62b633c10d99a38c8122bff49d4a1cb91fe6ae
Nabid Kaisar <himelkaisar023@gmail.com>
2022-10-23 21:03:48 +0600

    1. Intro component created 2. updated project_ideas.txt 3. added prettier

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,41 @@
+export default function Intro(){
+    return (
+        <>
+            <main>
+                <header>
+                   <h4> Hello, This is Nabid Kaisar, A professional front-end software engineer with 4 years of experience.</h4>
+                </header>
+                <p>
+                    <em>Expertise: JS, TS, React</em>
+                </p>
+            </main>
+
+
+            <header>Experiences:</header>
+            <br />
+            <section>
+                <header>
+                    Kona Software Lab Ltd.
+                </header>
+                <p>Software engineer level - 2</p>
+                <ul>
+                    <li>nagad</li>
+                    <li>lottery</li>
+                </ul>
+                <p>react, ts, js, vanilla js, next js, java</p>
+            </section>
+
+            <section>
+                <header>
+                    Synchronous ICT
+                </header>
+                <p>Assistant software Engineer</p>
+                <ul>
+                    <li>Synopi</li>
+                    <li>plagiarism checker</li>
+                </ul>
+                <p>react, js, vanilla js, node.js</p>
+            </section>
+        </>
+    )
+}
